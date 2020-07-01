ac0ac376480f138ddff62f348e525cdf975ed890
心飞为你飞 <747011882@qq.com>
2020-07-01 17:18:14 +0800

    Initial commit

---
+++ common/service_base/src/main/java/com/liuscoding/servicebase/exceptionhandler/exception/GuiException.java
@@ -0,0 +1,62 @@
+package com.liuscoding.servicebase.exceptionhandler.exception;
+
+
+import com.liuscoding.commonutils.result.ResultCode;
+import lombok.Data;
+
+/**
+ * @className: GuliException
+ * @description: 自定义异常处理
+ * @author: liusCoding
+ * @create: 2020-05-02 13:01
+ */
+
+@Data
+public class GuiException extends RuntimeException {
+
+    private Integer code;
+
+    private String msg;
+
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    private GuiException(Integer code, String msg) {
+        super(msg);
+        this.code = code;
+        this.msg = msg;
+    }
+
+    private GuiException(String msg) {
+        super(msg);
+        this.msg = msg;
+    }
+
+    public static GuiException from(ResultCode resultCode) {
+        return new GuiException(resultCode.getCode(), resultCode.getMsg());
+    }
+
+
+    public static GuiException from(String msg) {
+        return new GuiException(msg);
+    }
+
+    @Override
+    public String toString() {
+        return "GuiException{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
+
+}
